eef7311f2a3678390b8acec40109e16b49557bbc
Daniil <orlov121.121@mail.ru>
2022-11-25 18:23:07 +0300

    add src/cli.js

---
+++ src/cli.js
@@ -0,0 +1,6 @@
+import readlineSync from 'readline-sync';
+export function userName() {
+	console.log('Welcome to the Brain Games!');
+	const name = readlineSync.question('May I have your name? ');
+	return `Hello, ${name}`;
+}
