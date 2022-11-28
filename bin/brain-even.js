3962a5b5283e79c142a834c11dbf40e38704e517
Daniil <orlov121.121@mail.ru>
2022-11-28 15:34:01 +0300

    add bin/brain-even.js

---
+++ bin/brain-even.js
@@ -0,0 +1,31 @@
+#!/usr/bin/env node
+import {userName} from "../src/cli.js";
+import readlineSync from 'readline-sync';
+
+var name = userName();
+
+console.log(`Hello, ${name}!`);
+console.log(`Answer "yes" if the number is even, otherwise answer "no".`);
+
+for (var i = 0; i < 3; i++) {
+var random = Math.floor(Math.random() * 21),
+otvet = '',
+yesOrNo = '';
+
+console.log(`Question: ${random}`);
+otvet = readlineSync.question(`Your answer: `);
+yesOrNo = random % 2 === 0 ? 'yes' : 'no';
+
+if (yesOrNo === otvet) {
+    console.log('Correct!');
+} else {
+    console.log(`'yes' is wrong answer ;(. Correct answer was 'no'.`);
+    console.log(`Let's try again, ${name}`);
+    break;
+}
+
+if (i === 2) {
+    console.log(`Congratulations, ${name}!`);
+}
+
+}
