dba19abaef77645d34f0f3e5d8032ca49a4340c1
Орлов Даниил <orlov121.121@mail.ru>
2022-11-28 09:28:36 +0300

    fix src/cli.js

--- src/cli.js
+++ src/cli.js
@@ -2,5 +2,5 @@ import readlineSync from 'readline-sync';
 export function userName() {
 	console.log('Welcome to the Brain Games!');
 	const name = readlineSync.question('May I have your name? ');
-	return `Hello, ${name}`;
+	return `Hello, ${name}!`;
 }
