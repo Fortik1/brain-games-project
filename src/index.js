73f2ff84c70fd9f4f0a69a772e73b5acf7ca10a0
Daniil <orlov121.121@mail.ru>
2022-11-29 12:55:04 +0300

    add function NOD

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 export function randomNumber(max = 30) {
     return Math.floor(Math.random() * max);
 }
+
+export function NOD (x, y) {
+    if (y > x) return NOD(y, x);
+    if (!y) return x;
+    return NOD(y, x % y);
+}
