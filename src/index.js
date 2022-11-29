3fa7f46b4d6db9578c48af4bafd7b0452ca8139a
Daniil <orlov121.121@mail.ru>
2022-11-29 13:07:38 +0300

    fix src/index.js

--- src/index.js
+++ src/index.js
@@ -7,3 +7,12 @@ export function NOD (x, y) {
     if (!y) return x;
     return NOD(y, x % y);
 }
+
+export function neydacha(otvet, otvetUser, name) {
+    if (Number(otvetUser) === otvet) {
+        console.log(`Correct!`);
+    } else {
+        console.log(`'${otvetUser}' if wrong answer  ;(. Correct answer was '${otvet}'. \nLet's try again, ${name}!`);
+        return 1;
+    }
+}
