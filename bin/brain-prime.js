663e0b98c25e39c4318cf3bcc33ce2e784df510b
Орлов Даниил <orlov121.121@mail.ru>
2022-12-02 10:03:27 +0300

    add bin/brain-prime.js

---
+++ bin/brain-prime.js
@@ -0,0 +1,17 @@
+#!/usr/bin/env node
+import {userName} from "../src/cli.js";
+import {randomNumber, neydacha, Congratulations, primeNumber} from "../src/index.js";
+import readlineSync from "readline-sync";
+
+var name = userName();
+
+console.log(`Hello, ${name}!`);
+console.log(`Answer "yes" if given number is prime. Otherwise answer "no".`);
+
+for (var i = 0; i < 3; i++) {
+    var otvet = primeNumber(),
+    otvetUser = readlineSync.question("Your answer: "),
+    res = neydacha(otvet, otvetUser, name);
+    if (res === 1) break;
+    Congratulations(name, i);
+}
