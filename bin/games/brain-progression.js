d2cd8d077d1bab5534bc0e7f0973ed7054376cf5
Орлов Даниил <orlov121.121@mail.ru>
2022-12-02 12:01:40 +0300

    fix src/index.js

--- bin/games/brain-progression.js
+++ bin/games/brain-progression.js
@@ -1,4 +1,5 @@
-#!/usr/bin/env node
+#!/usr/bin/env node 
+/*
 import {randomNumber, neydacha, Congratulations, createArray} from "../../src/index.js";
 import {userName} from "../../src/cli.js";
 import readlineSync from "readline-sync";
@@ -24,4 +25,4 @@ for (var i = 0; i < 3; i++) {
     Congratulations(name, i);
     
 }
-
+*/
