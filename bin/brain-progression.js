ef0de787c5d486c34af808011598f1c7e10ca862
Орлов Даниил <orlov121.121@mail.ru>
2022-12-02 12:50:01 +0300

    fix brain-progress

--- bin/brain-progression.js
+++ bin/brain-progression.js
@@ -1,23 +1,26 @@
 #!/usr/bin/env node 
-import {randomNumber, neydacha, Congratulations, createArray} from "../src/index.js";
+import {NOD, randomNumber, neydacha, Congratulations} from "../src/index.js";
 import {userName} from "../src/cli.js";
 import readlineSync from "readline-sync";
 
 var name = userName();
 
 console.log(`Hello, ${name}!`);
-console.log(`What number is missing in the progression?`);
+console.log(`Find the greatest common divisor of given numbers.`);
 
 for (var i = 0; i < 3; i++) {
-    var otvet = createArray(),
-    otvetUser = readlineSync.question(`Your answer: `);
+    var numberOne = randomNumber(100) + 1,
+    numberTwo = randomNumber(100) + 1,
+    otvet = NOD(numberOne, numberTwo);
     
+    console.log(`Question: ${numberOne} ${numberTwo}`);
+    var otvetUser = readlineSync.question(`Your answer: `);
+
     var result = neydacha(otvet, otvetUser, name);
-    
+
     if (result === 1) {
         break;
     }
-    
+
     Congratulations(name, i);
-    
-}
+} 
